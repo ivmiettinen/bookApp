d26f0ed764063fc3f13a7889ff455f7f6679099b
Ilkka <55871738+ivmiettinen@users.noreply.github.com>
2021-05-21 09:25:43 +0300

    added background img, modified css

--- bookapp/src/components/MainHeader.js
+++ bookapp/src/components/MainHeader.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import classes from './MainHeader.module.css'
+// import bookImage from '../../pexels-negative-space-34592.jpg'
+import bookImage from './Pics/pexels-negative-space-34592.jpg'
 
 const MainHeader = () => {
     return (
         <header className={classes.header}>
             <ul>
                 <li>
-                    <NavLink activeClassName={classes.active} to='/auth'>Auth</NavLink>
+                    <NavLink activeClassName={classes.active} to='/auth'>
+                        Auth
+                    </NavLink>
                 </li>
                 <li>
-                    <NavLink activeClassName={classes.active} to='/books'>Books</NavLink>
+                    <NavLink activeClassName={classes.active} to='/books'>
+                        Books
+                    </NavLink>
                 </li>
                 <li>
-                    <NavLink activeClassName={classes.active} to='/about'>About</NavLink>
+                    <NavLink activeClassName={classes.active} to='/about'>
+                        About
+                    </NavLink>
                 </li>
             </ul>
         </header>
@@ -21,3 +29,5 @@ const MainHeader = () => {
 }
 
 export default MainHeader
+
+// style={{backgroundImage: `url(${bookImage})`}}
