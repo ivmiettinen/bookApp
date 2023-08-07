ecef62546281ba915a5902ce6824c34a039e8152
Ilkka <55871738+ivmiettinen@users.noreply.github.com>
2023-08-07 20:27:14 +0300

    LoginForm tests: -Test for calling login

--- frontend/src/components/SignIn/LoginForm.test.js
+++ frontend/src/components/SignIn/LoginForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 // import userEvent from '@testing-library/user-event';
 import LoginForm from './LoginForm';
 
@@ -9,22 +9,59 @@ describe('LoginForm', () => {
     });
 
     it('displays "Sign up" button text when showSignUp prop is true', () => {
+
+        //Arrange:
         const handleLoginMock = jest.fn();
         const showSignUp = true;
 
+        //Act:
         render(<LoginForm handleLogin={handleLoginMock} showSignUp={showSignUp} />);
 
+        //Assert:
         const submitButton = screen.getByText('Sign up');
         expect(submitButton).toBeInTheDocument();
     });
 
     it('displays "Login" button text when showSignUp prop is false', () => {
+
+        //Arrange:
         const handleLoginMock = jest.fn();
         const showSignUp = false;
 
+        //Act:
         render(<LoginForm handleLogin={handleLoginMock} showSignUp={showSignUp} />);
 
+        //Assert:
         const submitButton = screen.getByText('Login');
         expect(submitButton).toBeInTheDocument();
+    });
+
+    it('calls handleLogin with correct user credentials on form submission', () => {
+
+        //Arrange:
+
+        const handleLoginMock = jest.fn();
+        const showSignUp = true;
+
+        //Act:
+        render(<LoginForm handleLogin={handleLoginMock} showSignUp={showSignUp} />);
+
+        const usernameInput = screen.getByLabelText(/username/i);
+        const emailInput = screen.getByLabelText(/email/i);
+        const passwordInput = screen.getByLabelText(/password/i);
+        const submitButton = screen.getByText('Sign up');
+
+        fireEvent.change(usernameInput, { target: { value: 'testuser' } });
+        fireEvent.change(emailInput, { target: { value: 'test@user.com' } });
+        fireEvent.change(passwordInput, { target: { value: 'TestPassw123!' } });
+
+        fireEvent.click(submitButton);
+
+        // Assert:
+        expect(handleLoginMock).toHaveBeenCalledWith({
+            username: 'testuser',
+            email: 'test@user.com',
+            password: 'TestPassw123!',
+        });
     });
 });
