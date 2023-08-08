0c3aee6ec1ac1cdeb11e35eb74e401045a925aea
Ilkka <55871738+ivmiettinen@users.noreply.github.com>
2023-08-08 20:44:36 +0300

    LoginForm tests: -Implemented tests for loginform password and username

--- frontend/src/components/SignIn/LoginForm.test.js
+++ frontend/src/components/SignIn/LoginForm.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+
 import LoginForm from './LoginForm';
 
 describe('LoginForm', () => {
@@ -58,10 +58,66 @@ describe('LoginForm', () => {
         fireEvent.click(submitButton);
 
         // Assert:
+        expect(handleLoginMock).toHaveBeenCalledTimes(1);
         expect(handleLoginMock).toHaveBeenCalledWith({
             username: 'testuser',
             email: 'test@user.com',
             password: 'TestPassw123!',
         });
     });
+
+    it('displays error message for too short username when signing up', () => {
+
+        //Arrange:
+        const handleLoginMock = jest.fn();
+        const setErrorMessageMock = jest.fn();
+        const showSignUp = true;
+
+        //Act:
+        render(<LoginForm handleLogin={handleLoginMock} showSignUp={showSignUp} setErrorMessage={setErrorMessageMock} />);
+
+        const usernameInput = screen.getByLabelText(/username/i);
+        const submitButton = screen.getByText('Sign up');
+
+        fireEvent.change(usernameInput, { target: { value: 'ab' } });
+        fireEvent.click(submitButton);
+
+        // Assert:
+        expect(setErrorMessageMock).toHaveBeenCalledTimes(1);
+        expect(setErrorMessageMock).toHaveBeenCalledWith('Username must be at least 3 characters long.');
+    });
+
+    it('displays error message for too weak password when signing up', () => {
+
+        //Arrange:
+        const handleLoginMock = jest.fn();
+        const setErrorMessageMock = jest.fn();
+        const showSignUp = true;
+
+        //Act:
+        render(
+            <LoginForm
+                handleLogin={handleLoginMock}
+                showSignUp={showSignUp}
+                setErrorMessage={setErrorMessageMock}
+            />
+        );
+
+        const usernameInput = screen.getByLabelText(/username/i);
+        const passwordInput = screen.getByLabelText(/password/i);
+        const emailInput = screen.getByLabelText(/email/i);
+        const submitButton = screen.getByText('Sign up');
+
+        fireEvent.change(usernameInput, { target: { value: 'tester' } });
+        fireEvent.change(emailInput, { target: { value: 'test@address.com' } });
+        fireEvent.change(passwordInput, { target: { value: 'testpass' } });
+        fireEvent.click(submitButton);
+
+        // Assert:
+        expect(setErrorMessageMock).toHaveBeenCalledTimes(1);
+        expect(setErrorMessageMock).toHaveBeenCalledWith(
+            'Password must be 8 letters long and have AT LEAST: 1 lowercase, 1 uppercase, 1 number and 1 symbol.'
+        );
+    });
+
 });
