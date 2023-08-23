fe9bdfd0c5f092d3b206183d616873e971995276
Ilkka <55871738+ivmiettinen@users.noreply.github.com>
2023-08-23 20:40:26 +0300

    New Slice: -Created new ui-slice

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
-
 import bookReducer from '../src/store/bookReducer';
+import uiSlice from '../src/store/ui-slice';
 
 const store = configureStore({
     reducer: {
         books: bookReducer,
+        ui: uiSlice.reducer
     },
 });
 
