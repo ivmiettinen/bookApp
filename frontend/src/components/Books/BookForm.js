cd6ad8f17b9db979391b61cb5a36a64a02bf8fc9
Ilkka <55871738+ivmiettinen@users.noreply.github.com>
2023-09-04 17:33:16 +0300

    BookForm: -Added Togglable andBookActions to BooForm component

--- frontend/src/components/Books/BookForm.js
+++ frontend/src/components/Books/BookForm.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
 import classes from './BookForm.module.css';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import Button from '../UI/Button';
+import BookActions from '../../store/BookActions';
+import Togglable from '../../components/UI/Togglable';
 
-const BookForm = ({ addBook }) => {
+const BookForm = () => {
     const [title, setTitle] = useState('');
     const [author, setAuthor] = useState('');
     const [url, setUrl] = useState('');
+
+    const { addBook, bookFormRef } = BookActions();
+
+    const bookRef = bookFormRef;
 
     const createBook = (e) => {
         e.preventDefault();
@@ -25,59 +31,65 @@ const BookForm = ({ addBook }) => {
     };
 
     return (
-        <div className={classes.bookFormStyle}>
-            <h2>Create a new book</h2>
+        <Togglable
+            buttonLabel="Add a book"
+            ref={bookRef}
+            className="hideWhenVisible"
+        >
+            <div className={classes.bookFormStyle}>
+                <h2>Create a new book</h2>
 
-            <form onSubmit={createBook}>
-                <p>
-                    <label>title:</label>
-                    <br />
-                    <input
-                        name="title"
-                        type="text"
-                        id="title"
-                        maxLength="30"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        required
-                    />
-                </p>
+                <form onSubmit={createBook}>
+                    <p>
+                        <label>title:</label>
+                        <br />
+                        <input
+                            name="title"
+                            type="text"
+                            id="title"
+                            maxLength="30"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            required
+                        />
+                    </p>
 
-                <p>
-                    <label>author:</label>
-                    <br />
-                    <input
-                        name="author"
-                        type="text"
-                        id="author"
-                        maxLength="20"
-                        value={author}
-                        onChange={(e) => setAuthor(e.target.value)}
-                        required
-                    />
-                </p>
-                <p>
-                    <label>url:</label>
-                    <br />
-                    <input name="url"
-                        type="url"
-                        id="url"
-                        maxLength="60"
-                        value={url}
-                        onChange={(e) => setUrl(e.target.value)} required />
+                    <p>
+                        <label>author:</label>
+                        <br />
+                        <input
+                            name="author"
+                            type="text"
+                            id="author"
+                            maxLength="20"
+                            value={author}
+                            onChange={(e) => setAuthor(e.target.value)}
+                            required
+                        />
+                    </p>
+                    <p>
+                        <label>url:</label>
+                        <br />
+                        <input name="url"
+                            type="url"
+                            id="url"
+                            maxLength="60"
+                            value={url}
+                            onChange={(e) => setUrl(e.target.value)} required />
 
-                    <br />
-                </p>
-                <Button className={classes.bookFormButton} type="submit">
-                    create
-                </Button>
-            </form>
-        </div>
+                        <br />
+                    </p>
+                    <Button className={classes.bookFormButton} type="submit">
+                        create
+                    </Button>
+                </form>
+            </div>
+        </Togglable>
     );
 };
 
-BookForm.propTypes = {
-    addBook: PropTypes.func.isRequired,
-};
+// BookForm.propTypes = {
+//     addBook: PropTypes.func.isRequired,
+// };
 
 export default BookForm;
