065d6fc137d4f0d29059d36b223de91ae1f7fdc3
ilkka.miettinen <ilkka.miettinen@avanade.com>
2023-09-20 18:25:46 +0300

    Netlify build

---
+++ backend/netlify/functions/app.js
@@ -0,0 +1,57 @@
+import serverless from 'serverless-http';
+
+const config = require('../../utils/config')
+
+const express = require('express')
+
+require('express-async-errors')
+
+const app = express()
+const cors = require('cors')
+
+const booksRouter = require('./books')
+
+const middleware = require('../../utils/middleware')
+
+const logger = require('../../utils/logger')
+
+const mongoose = require('mongoose')
+
+const usersRouter = require('./users')
+
+const loginRouter = require('./login')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose
+    .connect(config.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connection to MongoDB:', error.message)
+    })
+
+app.use(cors())
+
+app.use(express.static('build'))
+
+app.use(express.json())
+
+app.use(middleware.requestLogger)
+
+app.use(middleware.tokenExtractor)
+
+// app.use('/api/books', booksRouter)
+
+// app.use('/api/users', usersRouter)
+
+// app.use('/api/login', loginRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports.handler = serverless(app);
---
+++ backend/netlify/functions/books.js
@@ -0,0 +1,140 @@
+import express, { Router } from 'express';
+import serverless from 'serverless-http';
+const booksRouter = require('./controllers/books'); // Import your books route
+const middleware = require('../../utils/middleware');
+
+const api = express();
+
+//
+
+const booksRouter = require('express').Router()
+const Book = require('../../models/book')
+const User = require('../../models/user')
+const jwt = require('jsonwebtoken')
+
+api.use(express.json());
+api.use(middleware.requestLogger);
+api.use(middleware.tokenExtractor);
+
+const router = Router();
+// 
+
+router.get('/', async (request, response, next) => {
+    try {
+        const books = await Book.find({}, '-user')
+        if (books) {
+            response.json(books.map((allBooks) => allBooks.toJSON()))
+        } else {
+            response.status(404).end()
+        }
+    } catch (exception) {
+        next(exception)
+    }
+})
+
+router.get('/:id', async (request, response, next) => {
+    try {
+        const books = await Book.findById(request.params.id)
+
+        if (books) {
+            response.json(books.toJSON())
+        } else {
+            response.status(404).end()
+        }
+    } catch (exception) {
+        next(exception)
+    }
+})
+
+router.post('/', async (request, response, next) => {
+    const body = request.body
+
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+    if (!request.token || !decodedToken.id || request.token === null) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
+    const book = new Book({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes === undefined ? 0 : body.likes,
+        user: user._id,
+    })
+    try {
+        if (
+            body.title !== undefined &&
+            body.author !== undefined &&
+            body.url !== undefined
+        ) {
+            const savedBook = await book.save()
+
+            user.books = user.books.concat(savedBook._id)
+            await user.save()
+            response.json(savedBook.toJSON())
+        } else {
+            response.status(400).send('Bad request. Information missing.')
+        }
+    } catch (exception) {
+        next(exception)
+    }
+})
+
+router.delete('/:id', async (request, response, next) => {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+    if (!request.token || !decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
+    const book = await Book.findById(request.params.id)
+
+    if (user._id.toString() !== book.user.toString()) {
+        //Deleter of the blog is not the same user as blog's adder
+        return response.status(400).json({ error: 'invalid user' })
+    }
+
+    try {
+        await Book.findByIdAndRemove(request.params.id)
+        response.status(204).end()
+    } catch (exception) {
+        next(exception)
+    }
+})
+
+router.put('/:id', async (request, response, next) => {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    const userId = request.params.id
+
+    if (!request.token || !decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    try {
+        const book = await Book.findById(request.params.id)
+        const userIndex = book.likesbyId.indexOf(request.params.id)
+
+        if (userIndex === -1) {
+            // User has not liked the post, add the like
+            book.likesbyId.push(userId)
+            book.likes += 1
+        } else {
+            // User has already liked the post, remove the like
+            book.likesbyId.pull(userId)
+            book.likes -= 1
+        }
+
+        book.save()
+        response.json(book.toJSON())
+    } catch (exception) {
+        console.log('exception', exception)
+        next(exception)
+    }
+})
+
+router.use('/api/books', router);
+
+module.exports.handler = serverless(booksRouter);
