064e84dd2fd27ad55fb5bef571e0a1fe57b756b2
Ilkka <55871738+ivmiettinen@users.noreply.github.com>
2023-10-02 20:23:00 +0300

    Refactored book.js

--- backend/models/book.js
+++ backend/models/book.js
@@ -1,32 +1,24 @@
 const mongoose = require('mongoose');
 
-let Book;
+const bookSchema = new mongoose.Schema({
+    id: Number,
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    url: { type: String, required: true },
+    likes: Number,
+    likesbyId: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+});
 
-try {
-    Book = mongoose.model('Book');
-} catch (e) {
-    const bookSchema = new mongoose.Schema({
-        id: Number,
-        title: { type: String, required: true },
-        author: { type: String, required: true },
-        url: { type: String, required: true },
-        likes: Number,
-        likesbyId: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-        user: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-        },
-    });
+bookSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    },
+});
 
-    bookSchema.set('toJSON', {
-        transform: (document, returnedObject) => {
-            returnedObject.id = returnedObject._id.toString();
-            delete returnedObject._id;
-            delete returnedObject.__v;
-        },
-    });
-
-    Book = mongoose.model('Book', bookSchema);
-}
-
-module.export = Book;
+module.exports = mongoose.model('Book', bookSchema);
